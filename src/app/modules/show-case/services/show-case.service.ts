a1dd85c9a9759840bce88995c8df223643ff4cba
Matias Balestra <mbalestra@applica.dev>
2023-07-15 05:40:55 -0300

    comienzo

--- src/app/modules/show-case/services/show-case.service.ts
+++ src/app/modules/show-case/services/show-case.service.ts
@@ -16,62 +16,62 @@ export class ShowCaseService {
       {
         name: "Pudgy Penguin #1",
         price: 0.2,
-        image: "https://api.pudgypenguins.io/penguin/image/1"
+        image: "https://randomfox.ca/images/1.jpg"
       },
       {
         name: "Pudgy Penguin #2",
         price: 0.1,
-        image: "https://api.pudgypenguins.io/penguin/image/2"
+        image: "https://randomfox.ca/images/2.jpg"
       },
       {
         name: "Pudgy Penguin #3",
         price: 0.3,
-        image: "https://api.pudgypenguins.io/penguin/image/3"
+        image: "https://randomfox.ca/images/3.jpg"
       },
       {
         name: "Pudgy Penguin #4",
         price: 0.4,
-        image: "https://api.pudgypenguins.io/penguin/image/4"
+        image: "https://randomfox.ca/images/4.jpg"
       },
       {
         name: "Pudgy Penguin #5",
         price: 0.5,
-        image: "https://api.pudgypenguins.io/penguin/image/5"
+        image: "https://randomfox.ca/images/5.jpg"
       },
       {
         name: "Pudgy Penguin #6",
         price: 0.6,
-        image: "https://api.pudgypenguins.io/penguin/image/6"
+        image: "https://randomfox.ca/images/6.jpg"
       },
       {
         name: "Pudgy Penguin #7",
         price: 0.7,
-        image: "https://api.pudgypenguins.io/penguin/image/7"
+        image: "https://randomfox.ca/images/7.jpg"
       },
       {
         name: "Pudgy Penguin #8",
         price: 0.8,
-        image: "https://api.pudgypenguins.io/penguin/image/8"
+        image: "https://randomfox.ca/images/8.jpg"
       },
       {
         name: "Pudgy Penguin #9",
         price: 0.9,
-        image: "https://api.pudgypenguins.io/penguin/image/9"
+        image: "https://randomfox.ca/images/9.jpg"
       },
       {
         name: "Pudgy Penguin #10",
         price: 0.9,
-        image: "https://api.pudgypenguins.io/penguin/image/10"
+        image: "https://randomfox.ca/images/10.jpg"
       },
       {
         name: "Pudgy Penguin #11",
         price: 0.9,
-        image: "https://api.pudgypenguins.io/penguin/image/11"
+        image: "https://randomfox.ca/images/11.jpg"
       },
       {
         name: "Pudgy Penguin #12",
         price: 0.9,
-        image: "https://api.pudgypenguins.io/penguin/image/12"
+        image: "https://randomfox.ca/images/12.jpg"
       }
     ]
 
---
+++ src/app/state/actions/items.actions.ts
@@ -0,0 +1,14 @@
+import { ItemModel } from '@core/models/Item.interface';
+import { createAction, props } from '@ngrx/store';
+
+
+
+export const loadItems = createAction(
+  '[Item List] Load items'
+);
+
+
+export const loadedItems = createAction(
+  '[Item List] Loaded success',
+  props<{items: ItemModel[] }>()
+);
